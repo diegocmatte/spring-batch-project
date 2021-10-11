3142dcd338b9dbd051a45ab130c6a2385bbabd21
Diego Matté <diegocmatte@gmail.com>
2021-10-11 15:22:44 -0300

    parametrizando os jobs

--- src/main/java/com/example/springbatchproject/BatchConfig.java
+++ src/main/java/com/example/springbatchproject/BatchConfig.java
@@ -6,11 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,15 +35,22 @@ public class BatchConfig {
                 .build();
     }
 
+
     // com lambda
     public Step imprimeOlaStep(){
         return stepBuilderFactory
                 .get("imprimeOlaStep")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("Ola Mundo");
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(imprimeOlaTasklet(null))
                 .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome){
+        return (stepContribution, chunkContext) -> {
+            System.out.println(String.format("Olá %s!", nome));
+            return RepeatStatus.FINISHED;
+        };
     }
 
     /* sem lamba
